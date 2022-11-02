099a14bc9821f55cd6dc48cbbdba1d4e33601a54
ShintaMorikawa <Shinta5-18@yahoo.ne.jp>
2022-11-02 11:26:20 +0900

    Github lecture01-kadai2

--- new Prmn2022/src/lecture01/kadai2.java
+++ new Prmn2022/src/lecture01/kadai2.java
@@ -1,4 +1,13 @@
 package lecture01;
 
+import java.util.Scanner;
+
 public class kadai2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int StudentNumber = input.nextInt();
+
+        System.out.println("b221" +StudentNumber + " Shinta Morikawa");
+    }
 }
